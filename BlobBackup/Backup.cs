c29721759ca9e040c0369c4bf22761eca32753bb
Anders Åberg <anders@andersaberg.com>
2017-09-15 17:10:12 +0200

    Added try/catch

--- BlobBackup/Backup.cs
+++ BlobBackup/Backup.cs
@@ -46,52 +46,61 @@ namespace BlobBackup
 
         public BackupJob PrepareJob(string containerName, string accountName, string accountKey, IProgress<int> progress)
         {
+
             Directory.CreateDirectory(Path.Combine(_localPath, containerName));
 
             var job = new BackupJob();
-            var account = CloudStorageAccount.Parse($"DefaultEndpointsProtocol=https;AccountName={accountName};AccountKey={accountKey};EndpointSuffix=core.windows.net");
-            var client = account.CreateCloudBlobClient();
-            var container = client.GetContainerReference(containerName);
             var allRemoteFiles = new HashSet<string>();
 
-            foreach (IListBlobItem blobItem in container.ListBlobs(null, true, BlobListingDetails.None))
+            try
             {
-                job.ScannedItems++;
-                if(job.ScannedItems % 50000 == 0)
-                {
-                    progress.Report(job.ScannedItems);
-                }
-                
-                if (blobItem is CloudBlockBlob)
-                {
-                    var localFileName = GetLocalFileName(_localPath, blobItem.Uri);
-                    allRemoteFiles.Add(localFileName);
-                    CloudBlockBlob blob = blobItem as CloudBlockBlob;
+                var account = CloudStorageAccount.Parse($"DefaultEndpointsProtocol=https;AccountName={accountName};AccountKey={accountKey};EndpointSuffix=core.windows.net");
+                var client = account.CreateCloudBlobClient();
+                var container = client.GetContainerReference(containerName);
 
-                    var file = new FileInfo(localFileName);
-
-                    if (file.Exists)
+                foreach (IListBlobItem blobItem in container.ListBlobs(null, true, BlobListingDetails.None))
+                {
+                    job.ScannedItems++;
+                    if (job.ScannedItems % 50000 == 0)
                     {
-                        if (file.LastWriteTime < blob.Properties.LastModified || file.Length != blob.Properties.Length)
+                        progress.Report(job.ScannedItems);
+                    }
+
+                    if (blobItem is CloudBlockBlob)
+                    {
+                        var localFileName = GetLocalFileName(_localPath, blobItem.Uri);
+                        allRemoteFiles.Add(localFileName);
+                        CloudBlockBlob blob = blobItem as CloudBlockBlob;
+
+                        var file = new FileInfo(localFileName);
+
+                        if (file.Exists)
                         {
-                            job.ModifiedFiles.Add(blob);
-                            job.ModifiedFilesSize += blob.Properties.Length;
+                            if (file.LastWriteTime < blob.Properties.LastModified || file.Length != blob.Properties.Length)
+                            {
+                                job.ModifiedFiles.Add(blob);
+                                job.ModifiedFilesSize += blob.Properties.Length;
+                            }
+                            else
+                            {
+                                job.UpToDateItems++;
+                            }
                         }
                         else
                         {
-                            job.UpToDateItems++;
+                            job.NewFiles.Add(blob);
+                            job.NewFilesSize += blob.Properties.Length;
                         }
                     }
                     else
                     {
-                        job.NewFiles.Add(blob);
-                        job.NewFilesSize += blob.Properties.Length;
+                        job.IgnoredItems++;
                     }
                 }
-                else
-                {
-                    job.IgnoredItems++;
-                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"EXCEPTION ({containerName}) #{job.ScannedItems}: " + ex.Message);
             }
 
             // scan for deleted files by checking if we have a file in the local file system that we did not find remotely
@@ -118,7 +127,7 @@ namespace BlobBackup
             if (job.NewFiles.Count > 0)
             {
                 Console.Write("Downloading new files: ");
-                
+
                 foreach (var item in job.NewFiles)
                 {
                     await throttler.WaitAsync();
@@ -131,14 +140,11 @@ namespace BlobBackup
                             await item.DownloadToFileAsync(localFileName, FileMode.Create);
                             Console.Write(".");
                         }
-                        catch(StorageException ex)
+                        catch (StorageException ex)
                         {
                             // Swallow 404 exceptions.
                             // This will happen if the file has been deleted in the temporary period from listing blobs and downloading
-                            if(!ex.Message.Contains("404"))
-                            {
-                                throw;
-                            }
+                            Console.Write("Swallowed Ex: " + ex.Message);
                         }
                         finally
                         {
@@ -166,6 +172,12 @@ namespace BlobBackup
                             await item.DownloadToFileAsync(localFileName, FileMode.Create);
                             Console.Write("-");
                         }
+                        catch (StorageException ex)
+                        {
+                            // Swallow 404 exceptions.
+                            // This will happen if the file has been deleted in the temporary period from listing blobs and downloading
+                            Console.Write("Swallowed Ex: " + ex.Message);
+                        }
                         finally
                         {
                             throttler.Release();
@@ -175,7 +187,7 @@ namespace BlobBackup
 
                 Console.WriteLine();
             }
-            
+
             if (job.DeletedFiles.Count > 0)
             {
                 Console.Write("Deleting files: ");
