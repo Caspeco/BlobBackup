f657efba27481bbff5945909d8e76dbf7e1d2aa0
Christian Nilsson <christian.nilsson@caspeco.se>
2019-02-23 22:21:55 +0100

    Add ArchiveFilemover
    This moves files from one place to another with date based directories, and also includes file modification year in that path.
    Helper program to split things up before safely creating 7zip archives

---
+++ ArchiveFilemover/ArchiveFilemoverMain.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchiveFilemover
+{
+    public class ArchiveFilemoverMain
+    {
+        public static int Main(string[] args)
+        {
+            return MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        public static async Task<int> MainAsync(string[] args)
+        {
+            var options = new CommandOptions();
+            if (!CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+            }
+
+            var sw = Stopwatch.StartNew();
+            var items = await MoveFiles(options);
+            sw.Stop();
+
+            Console.WriteLine($"Done {items} items traversed in {sw.Elapsed.ToString()}");
+            if (Debugger.IsAttached) Console.ReadKey();
+            return 0;
+        }
+
+        private static readonly HashSet<string> HasCreatedDirectories = new HashSet<string>();
+
+        private static async Task<long> MoveFiles(CommandOptions options)
+        {
+            long totalItemsTraversed = 0;
+            int itemsSincePrint = 0;
+            var runStart = DateTime.UtcNow;
+            var runStartString = runStart.ToString("yyyyMMdd_HHmm");
+            var hasMaxWriteTimeOption = options.MaxWriteTime != DateTime.MinValue;
+            var dir = new DirectoryInfo(options.SourcePath);
+            foreach (var fi in dir.EnumerateFiles("*", SearchOption.AllDirectories))
+            {
+                totalItemsTraversed++;
+                itemsSincePrint++;
+                if (itemsSincePrint % 10000 == 1)
+                {
+                    Console.Write('.');
+                }
+
+                try
+                {
+                    var lastWriteUtc = fi.LastWriteTimeUtc;
+                    if (hasMaxWriteTimeOption && lastWriteUtc > options.MaxWriteTime)
+                        continue;
+
+                    var destName = fi.FullName.Substring(options.SourcePath.Length + 1);
+                    var destFileName = Path.Combine(options.DestinationPath, runStartString + "_modyear_" + lastWriteUtc.Year, destName);
+                    var destdir = Path.GetDirectoryName(destFileName);
+
+                    var printDest = destdir.Substring(options.DestinationPath.Length + 1);
+                    Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff")} {destName} => {printDest}");
+                    itemsSincePrint = 0;
+
+                    if (!HasCreatedDirectories.Contains(destdir))
+                    {
+                        Directory.CreateDirectory(destdir);
+                        HasCreatedDirectories.Add(destdir);
+                    }
+
+                    fi.MoveTo(destFileName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception {fi.FullName} " + ex.ToString());
+                }
+            }
+
+            return totalItemsTraversed;
+        }
+    }
+}
---
+++ ArchiveFilemover/CommandOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace ArchiveFilemover
+{
+    class CommandOptions
+    {
+        [Option('s', "source", Required = true, HelpText = "Path to local directory where files to be backuped are stored")]
+        public string SourcePath { get; set; }
+
+        [Option('d', "destination", Required = true, HelpText = "Path to where moved files should be placed")]
+        public string DestinationPath { get; set; }
+
+        [Option('x', "maxwritedate", HelpText = "Ignore files with write time after this")]
+        public DateTime MaxWriteTime { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
